fe7a76c16c0ad14f3fe278642655ceed61a87c2f
NituDanielAlin <dan.alinnitu@gmail.com>
2022-04-06 20:41:45 +0300

    Created computeTax method and replace it in the calculateTotalTax

--- CtsHomework/src/ro/ase/csie/cts/g1099/design/paterns/models/Account.java
+++ CtsHomework/src/ro/ase/csie/cts/g1099/design/paterns/models/Account.java
@@ -5,8 +5,8 @@ import ro.ase.csie.cts.g1099.design.paterns.customexceptions.NegativeLoanExcepti
 public class Account {
 	private double	loanValue,rate;	
 	private int	daysActive;
-	private int DAYS_OF_YEAR = 365;
-	private double FEE_PERCENTAGE = .0125f;
+	private static int DAYS_OF_YEAR = 365;
+	private static double FEE_PERCENTAGE = .0125f;
 	AccountType type;
 	
 	public Account(double LoanValue, double Rate, AccountType Type) throws NegativeLoanException {
@@ -31,7 +31,7 @@ public class Account {
 		return loanValue*rate;
 	}
 	
-	public void setValue(double LoanValue) throws NegativeLoanException {
+	public void setLoanValue(double LoanValue) throws NegativeLoanException {
 		if(LoanValue<0)
 			throw new NegativeLoanException();
 		this.loanValue = LoanValue;
@@ -44,6 +44,12 @@ public class Account {
 				"; Type: "+ this.type+ ";";
 	}
 	
+	private static double computeTax(Account account) {
+		double tax = 0;
+		tax = FEE_PERCENTAGE*(account.loanValue*Math.pow(account.rate,(account.daysActive/DAYS_OF_YEAR)) - account.loanValue);
+		return tax;
+	}
+	
 	public static double calculateTotalTax(Account[] accounts)
 	{
 		double totalFee=0.0;
@@ -51,7 +57,7 @@ public class Account {
 		for	(int	i=0;i<accounts.length;i++){
 			account=accounts[i];
 			if(account.type.getTaxType() == 1)	
-				totalFee+= (account.loanValue*Math.pow(account.rate,(account.daysActive/365)) - account.loanValue);
+				totalFee+= computeTax(account);
 		}
 		return	totalFee;
 	}
