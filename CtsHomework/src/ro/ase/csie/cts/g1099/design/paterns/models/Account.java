bbe4dfc61ffb8152f6ec7f60ec1ee9d0be4d2ef3
NituDanielAlin <dan.alinnitu@gmail.com>
2022-04-06 20:46:15 +0300

    Created constants class and replaced the constant values in the calculateTotalTax method

--- CtsHomework/src/ro/ase/csie/cts/g1099/design/paterns/models/Account.java
+++ CtsHomework/src/ro/ase/csie/cts/g1099/design/paterns/models/Account.java
@@ -1,12 +1,11 @@
 package ro.ase.csie.cts.g1099.design.paterns.models;
-
+import ro.ase.csie.cts.g1099.design.paterns.models.Constants;
 import ro.ase.csie.cts.g1099.design.paterns.customexceptions.NegativeLoanException;
 
 public class Account {
 	private double	loanValue,rate;	
 	private int	daysActive;
-	private static int DAYS_OF_YEAR = 365;
-	private static double FEE_PERCENTAGE = .0125f;
+	static Constants constants;
 	AccountType type;
 	
 	public Account(double LoanValue, double Rate, AccountType Type) throws NegativeLoanException {
@@ -46,7 +45,7 @@ public class Account {
 	
 	private static double computeTax(Account account) {
 		double tax = 0;
-		tax = FEE_PERCENTAGE*(account.loanValue*Math.pow(account.rate,(account.daysActive/DAYS_OF_YEAR)) - account.loanValue);
+		tax = constants.FEE_PERCENTAGE*(account.loanValue*Math.pow(account.rate,(account.daysActive/constants.DAYS_OF_YEAR)) - account.loanValue);
 		return tax;
 	}
 	
@@ -54,7 +53,7 @@ public class Account {
 	{
 		double totalFee=0.0;
 		Account	account;
-		for	(int	i=0;i<accounts.length;i++){
+		for	(int i=0; i<accounts.length; i++){
 			account=accounts[i];
 			if(account.type.getTaxType() == 1)	
 				totalFee+= computeTax(account);
---
+++ CtsHomework/src/ro/ase/csie/cts/g1099/design/paterns/models/Constants.java
@@ -0,0 +1,6 @@
+package ro.ase.csie.cts.g1099.design.paterns.models;
+
+public class Constants {
+	public static int DAYS_OF_YEAR = 365;
+	public static double FEE_PERCENTAGE = .0125f;
+}
